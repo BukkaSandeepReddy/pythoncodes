fa69e0451de358d31a2d2972f0d7dee3f4fa0b7d
Sandeep Reddy <bukka.reddy@boomi.com>
2025-06-22 16:58:23 +0530

    Code fix in pallen

--- pallan.py
+++ pallan.py
@@ -1,9 +1,11 @@
+#This is Palindrome code
 def is_palindrome(text):
     text = text.lower().replace(" ", "")  # Normalize: lowercase & remove spaces
     return text == text[::-1]
 
 # Example usage
 word = "Madam"
+word1 = "RAJU"
 if is_palindrome(word):
     print(f"'{word}' is a palindrome.")
 else:
